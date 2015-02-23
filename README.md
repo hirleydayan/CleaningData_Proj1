b0faed6d47b6d4cb114f805d380f2765f3d24f5f
unknown <ehirday@BR00055446.ericsson.se>
2015-02-23 09:09:56 -0300

    Code and documentation cleanup.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # run_analysis.R
 
 run_analysis.R is a tool for reading and combining data from the 
-"Human Activity Recognition Using Smartphones Dataset" project (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones ).
+"Human Activity Recognition Using Smartphones Dataset" project at
+(http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones ).
 
-A direct link for the data is at:
+A direct link for the data is follows:
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 The script reads all training and testing data sets, and combines the information
@@ -14,3 +15,21 @@ measurements of mean and standard deviation.
 
 The script also groups by subject and activity  calculating the average of each 
 variable for each activity and each subject groupped.
+
+For downloading the data the followng function should be used:
+
+```{r}
+har.usd.data.download()
+
+```
+
+That function will download the data, unzip it and rename the folder to
+"UCI_HAR_Dataset".
+
+For processing the data and summarizing it on a single data set the following 
+function should be called:
+
+```{r}
+har.usd.summarize()
+
+```
--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,28 @@
-# download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-#              destfile = "getdata_projectfiles_UCI_HAR_Dataset.zip",
-#              method = "curl")
-# unzip("getdata_projectfiles_UCI_HAR_Dataset.zip")
-
 library("data.table")
 library("dplyr")
 
 #'
-#'This function reads all column names form features.txt file
-#'@param path The path to the features.txt file
+#' Helper function for downloading data set from the web and adjusting folder 
+#' name.
 #'
-get.data.header <- function(path) {
+har.usd.data.download <- function(){
+        download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                      destfile = "getdata_projectfiles_UCI_HAR_Dataset.zip",
+                      method = "curl")      
+        unzip("getdata_projectfiles_UCI_HAR_Dataset.zip")
+        file.rename("UCI HAR Dataset", "UCI_HAR_Dataset")
+}
+# 
+# 
+
+#'
+#' This function reads all column names form features.txt file
+#'
+#' @param path The path to the features.txt file
+#'
+#' @return The features vector
+#'
+get.data.feature <- function(path) {
         if (!exists("header")){
                 h <- read.table(paste(path, "features.txt", sep= "/"), 
                               stringsAsFactors = FALSE)
@@ -20,8 +32,11 @@ get.data.header <- function(path) {
 }
      
 #'
-#'This function reads all the activities from the activity_label.txt file.
-#'@param path The path to the activity_label.txt file 
+#' This function reads all the activities from the activity_label.txt file.
+#' 
+#' @param path The path to the activity_label.txt file 
+#' 
+#' @retun The activities \code{data.table}
 #'
 get.activities <- function(path) {
         
@@ -38,10 +53,14 @@ get.activities <- function(path) {
 }
 
 #'
-#'This function reads the data from, e.g training or testing, and
-#'combine data related to column names, activities and subjects.
-#'@param root.path The root path for the data
-#'@param set The set name that is also related to data subpath. 
+#' This function reads the data from, e.g training or testing, and
+#' combine data related to column names, activities and subjects.
+#' 
+#' @param root.path The root path for the data
+#' @param set The set name that is also related to data subpath.
+#'
+#' @return The full binded testing and training \code{data.table} 
+#' with column names, named categories and related observation subjects.
 #'
 get.data.table <- function(root.path, set){
         
@@ -49,7 +68,7 @@ get.data.table <- function(root.path, set){
         
         table <- read.table(paste(path, "/X_", set, ".txt", sep =""))
         table <- as.data.table(table)
-        table.header <- get.data.header(root.path)
+        table.header <- get.data.feature(root.path)
         setnames(table, table.header)
         
         ## Training subject column
@@ -77,28 +96,34 @@ get.data.table <- function(root.path, set){
         return(table.full)
 }
 
-# You should create one R script called run_analysis.R that does the following. 
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for 
-#    each measurement. 
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names. 
-training.dataset <- get.data.table("UCI_HAR_Dataset", "train")
-testing.dataset  <- get.data.table("UCI_HAR_Dataset", "test")
-
-filter.regex <- "subject|activity|std\\(\\)|mean\\(\\)"
-training.dataset.filtered <- select(training.dataset, matches(filter.regex))
-testing.dataset.filtered  <- select(testing.dataset, matches(filter.regex))
-
-dataset.filtered <- merge(training.dataset.filtered, 
-                          testing.dataset.filtered, 
-                          by = colnames(training.dataset.filtered), 
-                          all = TRUE)
-
-# 5. From the data set in step 4, creates a second, independent tidy data 
-#    set with the average of each variable for each activity and each subject. 
-dataset.f.grouped <- group_by(dataset.filtered, activity, subject)
-dataset.f.g.summarised <- summarise_each(dataset.f.grouped, funs(mean))
-write.table(dataset.f.g.summarised, file = "dataset.csv", row.name=FALSE)
+#'
+#' This function summarizes the data sets from the  "Human Activity Recognition 
+#' Using Smartphones Dataset" (HCA USD) project into a single data set.
+#' It  reads all training and testing data sets, and combines the information
+#' related to subjects and categories for each one of those data sets. 
+#' It merges both training and test data sets in a single set and filters all 
+#' the columns related to measurements of mean and standard deviation.
+#' It also groups by subject and activity  calculating the average of each 
+#' variable for each activity and each subject groupped.
+#' 
+#' @return The summarized HCA USD data set
+#' 
+har.usd.summarize <- function(){
+        training.dataset <- get.data.table("UCI_HAR_Dataset", "train")
+        testing.dataset  <- get.data.table("UCI_HAR_Dataset", "test")
+        
+        filter.regex <- "subject|activity|std\\(\\)|mean\\(\\)"
+        training.dataset.filtered <- select(training.dataset, matches(filter.regex))
+        testing.dataset.filtered  <- select(testing.dataset, matches(filter.regex))
+        
+        dataset.filtered <- merge(training.dataset.filtered, 
+                                  testing.dataset.filtered, 
+                                  by = colnames(training.dataset.filtered), 
+                                  all = TRUE)
+        dataset.f.grouped <- group_by(dataset.filtered, activity, subject)
+        dataset.f.g.summarised <- summarise_each(dataset.f.grouped, funs(mean))
+        write.table(dataset.f.g.summarised, file = "dataset.csv", row.name=FALSE)
+        return (dataset.f.g.summarised)
+}
 
 
